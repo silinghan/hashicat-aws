095e9f6f14526a35f9a61810fdd4af9c393004ae
silinghan <18482479+silinghan@users.noreply.github.com>
2021-10-13 15:26:44 +1100

    test

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. I am updating this file for testing terraform.
+  Welcome to ${PREFIX}'s app. I am updating this file for testing terraform v1.
   <!-- END -->
 
   </div>
